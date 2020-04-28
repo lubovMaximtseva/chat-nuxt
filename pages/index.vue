e7eee3204dd066cba5f5acb781d531a9e4f3afa0
Luba Maximtseva <lubov.maximtseva@gmail.com>
2020-04-28 10:22:19 +0300

    socket add

--- pages/index.vue
+++ pages/index.vue
@@ -2,32 +2,16 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8>
       <v-card min-width="400">
-        <v-card-title> <h1>Nuxt чат</h1></v-card-title>
+        <v-card-title>
+          <h1>Nuxt чат</h1>
+        </v-card-title>
         <v-card-text>
           <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field
-              v-model="name"
-              :counter="16"
-              :rules="nameRules"
-              label="Ваше имя"
-              required
-            ></v-text-field>
+            <v-text-field v-model="name" :counter="16" :rules="nameRules" label="Ваше имя" required></v-text-field>
 
-            <v-text-field
-              v-model="room"
-              :rules="roomRules"
-              label="Введите комнату"
-              required
-            ></v-text-field>
+            <v-text-field v-model="room" :rules="roomRules" label="Введите комнату" required></v-text-field>
 
-            <v-btn
-              :disabled="!valid"
-              color="primary"
-              class="mr-4"
-              @click="submit"
-            >
-              Войти
-            </v-btn>
+            <v-btn :disabled="!valid" color="primary" class="mr-4" @click="submit">Войти</v-btn>
           </v-form>
         </v-card-text>
       </v-card>
@@ -65,8 +49,16 @@ export default {
           name: this.name,
           room: this.room
         };
-        this.setUser(user);
-        this.$router.push("/chat");
+
+        this.$socket.emit("userJoined", user, data => {
+          if (typeof data === "string") {
+            console.error(data);
+          } else {
+            user.id = data.userId;
+            this.setUser(user);
+            this.$router.push("/chat");
+          }
+        });
       }
     }
   }
